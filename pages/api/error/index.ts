48ba7638527cf29588330a29e0054216f44e8bfc
Eliraas1 <eliraas1@ac.sce.ac.il>
2023-06-28 17:14:05 +0300

    new init app

--- pages/api/error/index.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(401).json({ message: "Not Authorized.", status: 401 });
-};
-
-export default handler;
