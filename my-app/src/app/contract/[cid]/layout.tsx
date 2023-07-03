1a7d8c79f37a62029a056748a1e9cc9bee01e9d3
Eliraas1 <eliraas1@ac.sce.ac.il>
2023-07-03 22:21:53 +0300

    deploy build fix

--- my-app/src/app/contract/[cid]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Providers from "@/app/Provider";
-
-interface ContractLayoutProps {
-  children: React.ReactNode;
-}
-
-export default function ContractLayout({ children }: ContractLayoutProps) {
-  return <Providers>{children}</Providers>;
-}
